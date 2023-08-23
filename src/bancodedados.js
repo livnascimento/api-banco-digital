4cf1d1f8cd3bd9d0fd05a50fb2225e2cf8c1c2d8
Lívia Nascimento <liviapnd@gmail.com>
2023-08-22 21:39:27 -0300

    removendo contas de teste

--- src/bancodedados.js
+++ src/bancodedados.js
@@ -5,32 +5,7 @@ const bancodedados = {
         agencia: '0001',
         senha: 'Cubos123Bank'
     },
-    contas: [
-        {
-            "numero": "343833521-1",
-            "saldo": 0,
-            "usuario": {
-                "nome": "livia",
-                "cpf": "26080892081",
-                "data_nascimento": "2001-11-12",
-                "telefone": "71999998899",
-                "email": "livia@gmail.com",
-                "senha": "123456"
-            }
-        },
-        {
-            "numero": "885748203-3",
-            "saldo": 0,
-            "usuario": {
-                "nome": "lucas",
-                "cpf": "13424967440",
-                "data_nascimento": "2001-11-12",
-                "telefone": "71999998899",
-                "email": "lucas@gmail.com",
-                "senha": "123456"
-            }
-        }
-    ],
+    contas: [],
     saques: [],
     depositos: [],
     transferencias: []
